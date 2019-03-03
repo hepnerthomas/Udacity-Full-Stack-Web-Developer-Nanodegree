4a05388aca4e2336a2fc7402ecd73552971991fe
Thomas Hepner <hepner.thomas@gmail.com>
2019-03-03 15:47:23 -0500

    fix: Remove unnecessary SSL import
    Remove SSL import from top of project.py file.

--- Project 4 - Item Catalog/vagrant/catalog/project.py
+++ Project 4 - Item Catalog/vagrant/catalog/project.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from flask import redirect, jsonify, url_for, flash
-from OpenSSL import SSL
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, AssetClass, FinancialAsset, User
